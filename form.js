d97b15dcec24e21fcdbcd6900a3b4aabbda55ae1
blips-design <bryan_lipscomb@yahoo.com>
2026-07-15 22:32:24 -0400

    Update form.js

--- form.js
+++ form.js
@@ -1,1 +1,21 @@
-console.log('form');
+document
+.getElementById("feedbackForm")
+.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    const data = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value
+    };
+
+    localStorage.setItem(
+        "feedback",
+        JSON.stringify(data)
+    );
+
+    alert("Form submitted successfully.");
+
+    this.reset();
+});
